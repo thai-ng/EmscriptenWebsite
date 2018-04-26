6ea793b96036e77261ef700961541b887dcdbe4b
Thai Quoc Nguyen <thaiquocnguyen@outlook.com>
2018-04-25 22:13:31 -0700

    work with project from MSVC now

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,2 @@
-wsl ./build.sh
-start ../build.emscripten/index.html
+%windir%\sysnative\wsl.exe ./scripts/build.sh
+start build.emscripten/index.html
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-mkdir -p ../build
-cd ../build
+mkdir -p build
+cd build
 cmake ../ -DCMAKE_TOOLCHAIN_FILE="../emsdk-linux/emscripten/1.37.38/cmake/Modules/Platform/Emscripten.cmake"
 make
